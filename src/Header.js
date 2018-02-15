63696b849b993d72f857d40d962168a5012f14bf
Pete K <krautscheid@mitre.org>
2018-02-15 12:52:04 -0500

    Add patient age and photo

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,25 @@
 import React from 'react';
 
 function Header(props) {
+  const patient = props.patient;
   let name = '';
   let id = '';
-  const patient = props.patient;
+  let age = '';
+  let img = ''
   if (patient) {
     name = patient.name;
     id = patient.id;
+    age = patient.age;
+    if (patient.photoSrc) {
+      img = <img src={patient.photoSrc} />;
+    }
   }
-  // TODO Add different info (maybe photo)
   return (
     <div className="infohead">
       Patient Name: <span className="data" id="patient_name">{name}</span><br/>
       Patient ID: <span className="data" id="patient_id">{id}</span><br/>
-      Patient Age: <span className="data" id="patient_age"></span><br/>
+      Patient Age: <span className="data" id="patient_age">{age}</span><br/>
+      {img}
     </div>
   );
 }
--- src/Patient.js
+++ src/Patient.js
@@ -11,6 +11,29 @@ class Patient {
     const last = this.resource.name[0].family;
     return `${first} ${last}`;
   }
+  get age() {
+    // Calculate either to date of death or to today
+    const startDate = this.resource.birthDate;
+    const endDate = this.resource.deceasedDateTime || (new Date()).toISOString();
+    const ageInSeconds = (Date.parse(endDate) - Date.parse(startDate)) / 1000;
+    return `${Math.round(ageInSeconds / (60*60*24*365))} years`;
+  }
+  get photoSrc() {
+    // Build a src string appropriate for putting into an img tag
+    if (this.resource.photo && this.resource.photo.length > 0) {
+      const contentType = this.resource.photo[0].contentType;
+      const base64 = this.resource.photo[0].data;
+      return `data:${contentType};base64, ${base64}`;
+    } else {
+      return false;
+    }
+  }
+ conditions() {
+    //var pt_id = smart.patient.id;
+    //smart.api.search({type: "Condition", query: {patient: pt_id}}).then(function(r) {
+    //  callback(null, r.data);
+    //})
+  }
 }
 
 export default Patient;
