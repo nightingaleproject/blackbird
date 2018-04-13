1daa487685e152da7bcee7555ba6827f5d78e25d
Pete K <krautscheid@mitre.org>
2018-04-13 15:16:24 -0400

    Search box for restricting resources in timeline view

--- src/Timeline.js
+++ src/Timeline.js
@@ -1,49 +1,72 @@
 import React, { Component } from 'react';
-import { Menu, Card } from 'semantic-ui-react';
+import { Menu, Card, Input } from 'semantic-ui-react';
 
 class Timeline extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { tab: "Conditions" };
+    this.state = {
+      tab: "Conditions",
+      displayedConditions: props.conditions.slice(),
+      displayedProcedures: props.procedures.slice(),
+      displayedObservations: props.observations.slice(),
+      displayedMedications: props.medications.slice()
+    };
     this.gotoTab = this.gotoTab.bind(this);
+    this.handleSearchChange = this.handleSearchChange.bind(this);
   }
 
   gotoTab(newTab) {
     this.setState({ tab: newTab });
   }
 
-  render() {
+  handleSearchChange(event, input) {
+    const regex = new RegExp(input.value, 'gi');
+    const filter = (element) => element.description.match(regex);
+    this.setState({
+      displayedConditions: this.props.conditions.filter(filter),
+      displayedProcedures: this.props.procedures.filter(filter),
+      displayedObservations: this.props.observations.filter(filter),
+      displayedMedications: this.props.medications.filter(filter)
+    });
+  }
 
-    const conditionLink = (condition) => {
-      return <Card header={condition.description} key={condition.id} onClick={this.props.handleConditionClick} id={condition.id} />;
-    };
+  render() {
 
     const conditionLinks = (conditions) => {
       if (!conditions || conditions.length === 0) {
-        return <div>No conditions found</div>;
+        return <p>No conditions found</p>;
       } else {
-        return conditions.map(conditionLink);
+        return conditions.map((condition) => {
+          return <Card fluid header={condition.description}
+                       key={condition.id}
+                       onClick={this.props.handleConditionClick}
+                       id={condition.id} />;
+        });
       }
     };
 
-    const resourceCards = (resources) => {
-      return resources.map((resource) => {
-        return <Card header={resource.description} key={resource.id} />;
-      });
+    const resourceCards = (name, resources) => {
+      if (!resources || resources.length === 0) {
+        return <p>No {name} found</p>;
+      } else {
+        return resources.map((resource) => {
+          return <Card fluid header={resource.description} key={resource.id} />;
+        });
+      }
     };
 
     const renderTab = (tab) => {
       switch (tab) {
       case 'Conditions':
       default:
-        return conditionLinks(this.props.conditions);
+        return conditionLinks(this.state.displayedConditions);
       case 'Procedures':
-        return resourceCards(this.props.procedures);
+        return resourceCards('procedures', this.state.displayedProcedures);
       case 'Tests':
-        return resourceCards(this.props.observations);
+        return resourceCards('tests', this.state.displayedObservations);
       case 'Medications':
-        return resourceCards(this.props.medications);
+        return resourceCards('medications', this.state.displayedMedications);
       }
     };
 
@@ -59,15 +82,15 @@ class Timeline extends Component {
     return (
       <div className="timeline">
         <Menu tabular>
-          {menuItem('Conditions', this.props.conditions)}
-          {menuItem('Procedures', this.props.procedures)}
-          {menuItem('Tests', this.props.observations)}
-          {menuItem('Medications', this.props.medications)}
+          {menuItem('Conditions', this.state.displayedConditions)}
+          {menuItem('Procedures', this.state.displayedProcedures)}
+          {menuItem('Tests', this.state.displayedObservations)}
+          {menuItem('Medications', this.state.displayedMedications)}
         </Menu>
+        <Input fluid icon='search' placeholder='Search...' onChange={this.handleSearchChange} />
         {renderTab(this.state.tab)}
       </div>
     );
-
   }
 }
 
