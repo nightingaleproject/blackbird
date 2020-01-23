c357eaf58740fa299407a3e4e4812a9ada961988
Adam M. Holmes <aholmes@mitre.org>
2020-01-23 10:35:39 -0500

    Added blackbird details

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,8 @@ import { Menu } from 'semantic-ui-react';
 function Header(props) {
   return (
     <Menu color='blue' inverted attached>
-      <Menu.Item header><h2>U.S. Standard Certificate of Death</h2></Menu.Item>
-      <Menu.Item name='about' position='right' />
+      <Menu.Item header><h2>Blackbird</h2></Menu.Item>
+      <Menu.Item name='About' position='right' onClick={() => window.location = 'https://github.com/nightingaleproject/blackbird'} />
     </Menu>
   );
 }
