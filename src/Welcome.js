8bf4d6ddd8e7fcd890400ce2f80f9620d6b6b77f
Pete K <krautscheid@mitre.org>
2020-02-18 12:06:01 -0500

    Add clarity on language describing the state of development

--- src/Welcome.js
+++ src/Welcome.js
@@ -20,7 +20,7 @@ class Welcome extends Component {
     return (
       <div>
         <h2>Welcome</h2>
-        <p>The purpose of this application is to provide visualization, context, and decision support at the point of a patient's death, with the aim of improving the timeliness, accuracy, and completeness of mortality reporting. Source code and documentation can be <a href="https://github.com/nightingaleproject/blackbird">found on GitHub</a>.</p>
+        <p>The purpose of this application is to demonstrate a technical approach for allowing medical certifiers to report and certify to jurisdiction electronic death registration systems (EDRS) from a hospital setting. Note that this is only intended as a technical demonstration; no testing has been performed to determine potential impact on data quality. Source code and documentation can be <a href="https://github.com/nightingaleproject/blackbird">found on GitHub</a>.</p>
         <Menu pointing>
           <Menu.Item name='plain'
                      active={selectedMenuItem === 'plain'}
